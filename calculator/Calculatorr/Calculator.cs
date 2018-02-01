846427ef692525082b7d57c8c1d2128fbb432f6a
nicho1991 <ladefoged1991@gmail.com>
2018-02-01 11:12:16 +0100

    round

--- calculator/Calculatorr/Calculator.cs
+++ calculator/Calculatorr/Calculator.cs
@@ -26,13 +26,9 @@ namespace Calculator
 
         public double Power(double x, double exp)
         {
-<<<<<<< HEAD
-            double roundup = Math.Pow(x, exp);
-            return Math.Round(roundup, 3);
-=======
             Accumulator = Math.Pow(x, exp);
-            return Accumulator;
->>>>>>> 4dbb4e78b33252567870b4792e0b4ca7663480b5
+            return Math.Round(Accumulator,3);
+
         }
 
         public double Divide(double dividend, double divisor)
