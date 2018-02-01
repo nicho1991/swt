2b9764f0a77d2966986227b81a11fc57079e7210
nicho1991 <ladefoged1991@gmail.com>
2018-02-01 10:07:14 +0100

    divide

--- calculator/Calculatorr/Calculator.cs
+++ calculator/Calculatorr/Calculator.cs
@@ -23,5 +23,12 @@ namespace Calculator
         {
             return Math.Pow(x, exp);
         }
+
+        public double Divide(double dividend, double divisor)
+        {
+            if (divisor == 0)
+                throw new DivideByZeroException();
+            return dividend / divisor;
+        }
     }
 }
