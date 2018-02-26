533e25d6e3048248e6c7929bd7bb6948c8aa82d9
nicho1991 <ladefoged1991@gmail.com>
2018-02-26 17:56:00 +0100

    Update

---
+++ eksempler/SWT_EventsAndNSubstitute/ECSWithEvents.Test.Unit/ECSWithEventsUnitTests.cs
@@ -0,0 +1,51 @@
+﻿using NSubstitute;
+using NUnit.Framework;
+
+namespace ECSWithEvents.Test.Unit
+{
+    [TestFixture]
+    public class ECSWithEventsUnitTests
+    {
+        private int _increaseTempEventsReceived;
+        private int _decreaseTempEventsreceived;
+        private ITempSensor _tempSensor;
+        private Control _uut;
+
+        [SetUp]
+        public void Setup()
+        {
+            // Initialize event counters
+            _increaseTempEventsReceived = 0;
+            _decreaseTempEventsreceived = 0;
+
+            _tempSensor = Substitute.For<ITempSensor>();
+            _uut = new Control(_tempSensor, 25);    // Control should hook up to our temperature source 
+
+            // Initialize anonymous event listeners that listen to events from Control
+            // When an event occurs, increase the associated event counter.
+            // See e.g. http://nsubstitute.github.io/help/raising-events/
+            _uut.IncreaseTempEvent += (sender, args) => { ++_increaseTempEventsReceived; };
+            _uut.DecreaseTempEvent += (sender, args) => { ++_decreaseTempEventsreceived; };
+
+        }
+
+        [TestCase(26, 1)]
+        [TestCase(25, 1)]
+        [TestCase(24, 0)]
+        public void RegulateTemp_VariousTemps_DecreaseTempEventsRaisedAsExpected(int temp, int nTimesDecreaseEventExpected)
+        {
+            _tempSensor.TempChangedEvent += Raise.EventWith(new TempChangedEventArgs {Temp = temp});    // Raise TempChangedEvent through fake
+            Assert.That(_decreaseTempEventsreceived, Is.EqualTo(nTimesDecreaseEventExpected));          // Asert that event from UUT was received
+        }
+
+
+        [TestCase(26, 0)]
+        [TestCase(25, 0)]
+        [TestCase(24, 1)]
+        public void RegulateTemp_VariousTemps_IncreaseTempEventsRaisedAsExpected(int temp, int nTimesIncreaseEventExpected)
+        {
+            _tempSensor.TempChangedEvent += Raise.EventWith(new TempChangedEventArgs { Temp = temp });
+            Assert.That(_increaseTempEventsReceived, Is.EqualTo(nTimesIncreaseEventExpected));
+        }
+    }
+}
---
+++ eksempler/SWT_EventsAndNSubstitute/ECSWithEvents/Control.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace ECSWithEvents
+{
+    public class Control
+    {
+        public Control(ITempSensor tempSensor, int threshold)
+        {
+            tempSensor.TempChangedEvent += HandleTempChangedEvent;
+            Threshold = threshold;
+        }
+
+        public event EventHandler IncreaseTempEvent;
+        public event EventHandler DecreaseTempEvent;
+
+        public int Threshold { get; set; }
+
+        public void HandleTempChangedEvent(object source, TempChangedEventArgs args)
+        {
+            if(args.Temp < Threshold) OnIncreaseTempEvent();
+            else OnDecreaseTempEvent();
+        }
+
+        protected virtual void OnIncreaseTempEvent()
+        {
+            IncreaseTempEvent?.Invoke(this, EventArgs.Empty);
+        }
+
+        protected virtual void OnDecreaseTempEvent()
+        {
+            DecreaseTempEvent?.Invoke(this, EventArgs.Empty);
+        }
+    }
+
+}
